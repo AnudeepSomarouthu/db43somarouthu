c6cb926bfbbcb91fed92a8c7a2ea6f0860b99da4
AnudeepSomarouthu <89408141+AnudeepSomarouthu@users.noreply.github.com>
2021-11-18 21:42:37 -0600

    committed

--- controllers/pen.js
+++ controllers/pen.js
@@ -37,8 +37,16 @@ exports.pen_detail = async function (req, res) {
 };
  
 // Handle pen delete form on DELETE. 
-exports.pen_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: pen delete DELETE ' + req.params.id); 
+exports.pen_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await pen.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
  
 // Handle pen update form on PUT. 
@@ -83,3 +91,59 @@ exports.pen_create_post = async function(req, res) {
     }   
 }; 
 
+// Handle a show one view with id specified by query 
+exports.pen_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await pen.findById( req.query.id) 
+        res.render('pendetail',  
+{ title: 'Pen Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.pen_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('pencreate', { title: 'Pen Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.pen_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await pen.findById(req.query.id) 
+        res.render('penupdate', { title: 'Pen Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.pen_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Costume.findById(req.query.id) 
+        res.render('pendelete', { title: 'Pen Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
--- routes/pen.js
+++ routes/pen.js
@@ -1,8 +1,16 @@
 var express = require('express');
-const pen_controllers= require('../controllers/pen'); 
+const pen_controlers= require('../controllers/pen'); 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', pen_controllers.pen_view_all_Page);
-
+router.get('/', pen_controlers.pen_view_all_Page);
 module.exports = router;
+/* GET detail pen page */ 
+router.get('/detail', pen_controlers.pen_view_one_Page); 
+/* GET create pen page */ 
+router.get('/create', pen_controlers.pen_create_Page); 
+/* GET create update page */ 
+router.get('/update', pen_controlers.pen_update_Page); 
+/* GET create costume page */ 
+router.get('/delete', pen_controlers.pen_delete_Page); 
+
