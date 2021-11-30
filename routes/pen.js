9ac0ecb149a62dc21747d2403f47286654be891a
AnudeepSomarouthu <89408141+AnudeepSomarouthu@users.noreply.github.com>
2021-11-30 00:14:52 -0600

    Added update secured

--- routes/pen.js
+++ routes/pen.js
@@ -2,6 +2,17 @@ var express = require('express');
 const pen_controlers= require('../controllers/pen'); 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+// or;
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  };
+
 /* GET home page. */
 router.get('/', pen_controlers.pen_view_all_Page);
 module.exports = router;
@@ -10,7 +21,7 @@ router.get('/detail', pen_controlers.pen_view_one_Page);
 /* GET create pen page */ 
 router.get('/create', pen_controlers.pen_create_Page); 
 /* GET create update page */ 
-router.get('/update', pen_controlers.pen_update_Page); 
+router.get('/update',secured, pen_controlers.pen_update_Page); 
 /* GET create costume page */ 
 router.get('/delete', pen_controlers.pen_delete_Page); 
 
