3960bdf8024ae0c482331b7ecd537b0799bcb5d9
AnudeepSomarouthu <89408141+AnudeepSomarouthu@users.noreply.github.com>
2021-11-30 14:53:20 -0600

    Added buttons

--- models/pen.js
+++ models/pen.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose") 
 const penSchema = mongoose.Schema({  
-    type: String,  
+    type: {type:String, required:true},
     ink_color: String,  
     cost:  { type: Number, min: 5, max: 400, default: 0 },
 }); 
--- routes/pen.js
+++ routes/pen.js
@@ -19,9 +19,9 @@ module.exports = router;
 /* GET detail pen page */ 
 router.get('/detail', pen_controlers.pen_view_one_Page); 
 /* GET create pen page */ 
-router.get('/create', pen_controlers.pen_create_Page); 
+router.get('/create',secured, pen_controlers.pen_create_Page); 
 /* GET create update page */ 
 router.get('/update',secured, pen_controlers.pen_update_Page); 
 /* GET create costume page */ 
-router.get('/delete', pen_controlers.pen_delete_Page); 
+router.get('/delete',secured, pen_controlers.pen_delete_Page); 
 
